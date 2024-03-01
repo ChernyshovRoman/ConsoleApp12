e733aa21656bed20e19d4fedbd68c9c8312a9ad8
chesnykh <bigkwest@mail.ru>
2024-03-01 15:31:32 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+            Console.WriteLine("Введите значение x");
+            x = double.Parse(Console.ReadLine());
+            y = 8 * Math.Sin(2 * x) - Math.Cos(2 * x);
+            Console.WriteLine("y = " + Math.Round(y, 4));
+            Console.ReadLine();
+        }
+    }
+}
